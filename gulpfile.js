23fdbf037cdc2eff93c91d971af5042db8e62c18
Arunjit Singh <opensrc@ajsd.in>
2014-07-08 16:06:30 +0530

    improve the builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,42 @@
 var path = require('path');
-
 var gulp = require('gulp');
+
+var filter = require('gulp-filter');
 var rimraf = require('gulp-rimraf');
 var roole = require('gulp-roole');
 var symlink = require('gulp-sym');
+var watch = require('gulp-watch');
 
 var SRC = 'src';
 var DEV = 'build/dev';
 var DIST = 'build/dist'
+
+function isAdded(file) {
+  return file.event === 'added';
+}
+
+function isModified(file) {
+  return file.event === 'changed';
+}
+
+function linkFile(file) {
+  return path.join(path.dirname(file.base), DEV, file.relative);
+}
 
 gulp.task('clean', function() {
   return gulp.src('build', {read: false})
       .pipe(rimraf({force: true}));
 });
 
+
 gulp.task('symlink', function() {
   return gulp.src(['src/**/*.*', '!src/**/*.roo']).
-    pipe(symlink(function(file) {
-      return path.join(path.dirname(file.base), DEV, file.relative);
-    }));
+    pipe(symlink(linkFile, {force: true}));
 });
 
 gulp.task('symlink-packages', function() {
-  return gulp.src('bower_components')
-      .pipe(symlink(path.join(DEV, 'packages')));
+  return gulp.src('bower_components', {read: false})
+      .pipe(symlink(path.join(DEV, 'packages'), {force: true}));
 });
 
 gulp.task('roole', function() {
@@ -32,8 +45,17 @@ gulp.task('roole', function() {
       .pipe(gulp.dest(DEV));
 });
 
-gulp.task('watch', function() {
-  return gulp.watch('src/**/*.roo', ['roole']);
+gulp.task('watch-srcs', function() {
+  return watch({glob: ['src/**/*.*', '!src/**/*.roo'], name: 'srcs'})
+      .pipe(filter(isAdded))
+      .pipe(symlink(linkFile, {force: true}));
 });
 
-gulp.task('dev', ['symlink', 'symlink-packages', 'roole']);
+gulp.task('watch-roole', function() {
+  return watch({glob: ['src/**/*.roo'], name: 'roole'})
+      .pipe(filter(isModified))
+      .pipe(roole())
+      .pipe(gulp.dest(DEV));
+});
+
+gulp.task('dev', ['symlink', 'symlink-packages', 'watch-roole', 'watch-srcs']);
