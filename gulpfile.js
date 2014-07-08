a644f3a857592f415881d67cebea643ade28d009
Arunjit Singh <opensrc@ajsd.in>
2014-07-08 12:35:19 +0530

    Create builder for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
+var path = require('path');
+
 var gulp = require('gulp');
 var roole = require('gulp-roole');
+var symlink = require('gulp-symlink');
 
-gulp.task('default', function() {
+var SRC = 'src';
+var DEV = 'dev';
+
+gulp.task('symlink', function() {
+  return gulp.src(['src/**/*.*', '!src/**/*.roo']).
+    pipe(symlink.absolute(function(file) {
+      return path.join(path.dirname(file.base), DEV, file.relative);
+    }));
+});
+
+gulp.task('roole', function() {
   return gulp.src(['src/**/*.roo'])
       .pipe(roole())
-      .pipe(gulp.dest('build'));
+      .pipe(gulp.dest(DEV));
 });
+
+gulp.task('watch', function() {
+  return gulp.watch('src/**/*.roo', ['roole']);
+});
+
+gulp.task('dev', ['symlink', 'roole']);
